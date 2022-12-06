ad8f19f84a76ab9c93215139d2e68738fb8b8e64
lufranski <lufranski@gmail.com>
2022-12-06 15:31:06 +0100

    Main ex

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            listOfEmails: []
+        }
+    },
+    methods: {
+        emailGenerator(){
+
+            for(let i = 0; i < 10; i++) {
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((result) => {
+                    
+                    this.getIntoArray(result.data.response);
+
+                });
+
+            }
+
+        },
+        getIntoArray(element){
+
+            this.listOfEmails.push(element);
+
+        }
+    },
+    mounted () {
+        
+        this.emailGenerator();
+
+    }
+}).mount('#app')
